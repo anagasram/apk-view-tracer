bde5a628cd8af576138726e1644587e33301350d
kun_ma <kun_ma@nj-kun-ma.tw.trendnet.org>
2012-05-08 11:34:19 +0800

    change some detail info in MonkeyRunnerImpl.py

--- MonkeyRunnerImpl.py
+++ MonkeyRunnerImpl.py
@@ -18,7 +18,7 @@ from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice, MonkeyImage
 from com.android.monkeyrunner.recorder import MonkeyRecorder
 
 class MonkeyRunnerImpl():
-    def __init__(self):
+    def __init__(self, serial=None):
         ## command map
         self.CMD_MAP = {"TOUCH": lambda dev, arg: dev.touch(**arg),
                         "DRAG": lambda dev, arg: dev.drag(**arg),
@@ -39,13 +39,14 @@ class MonkeyRunnerImpl():
                               "ENTER": "enter"
                             }
         self.action_type_list=["DOWN", "UP", "DOWN_AND_UP"]
+        self.action_down = "DOWN"
+        self.action_up = "UP"
+        self.action_down_and_up = "DOWN_AND_UP"
         
-        self.device = MonkeyRunner.waitForConnection()
+        self.device = MonkeyRunner.waitForConnection(deviceId=serial)
 
-    def action(self, key_code, action_type):
-        self.device.press(key_code, action_type)
-    
-    def clickByKeyCode(self, key_code, action_type):
+   
+    def press(self, key_code, action_type):
         self.device.press(key_code, action_type)
     
     def clickHomeButton(self):
@@ -58,29 +59,14 @@ class MonkeyRunnerImpl():
         self.device.press("KEYCODE_BACK", "DOWN_AND_UP")
     
     def clickSearchButton(self):
-        self.device.press("KEYCODE_SEARCH", "DOWN_AND_UP")
-    
+        self.device.press("KEYCODE_SEARCH", "DOWN_AND_UP")    
     
     def sleep(self, time_sec):
         MonkeyRunner.sleep(time_sec)
     
-    def drag(self, fromX, fromY, toX, toY):
-        pass
-
-    #def drag_Down(self, device):
-    #    cd = ChangeData
-    #    log = Logs()
-    #    list = cd.coordinate()
-    #    if len(list) > 0:
-    #        startX = list[0]
-    #        startY = list[1]
-    #        endX = list[2]
-    #        endY = list[3]
-    #        device.drag((startX,startY),(endX,endY),3,2)
-    #        log.writeLog('device.drag(('+str(startX)+','+str(startY)+'),('+str(endX)+','+str(endY)+'),'+str(3)+','+str(2)+')')
-    #    else:
-    #       log.writeLog('List is null') 
-    
+    #- Simulates a drag gesture (touch, hold, and move) on this device's screen.
+    def drag(self, fromX, fromY, toX, toY, duration=1.0, steps=10):
+        self.device.drag((fromX, fromY), (toX, toY), duration, steps)
     
     def touch(self, targetX, targetY, action_type):
         self.device.touch(targetX, targetY, action_type)
@@ -102,9 +88,15 @@ class MonkeyRunnerImpl():
         except Exception,e:
             print e
             return None
-        
-    def typeText(self):
-        pass
+    
+    #===========================================================================
+    # Sends the characters contained in message to this device, 
+    # as if they had been typed on the device's keyboard. 
+    # This is equivalent to calling press() for each keycode in message 
+    # using the key event type DOWN_AND_UP.    
+    #===========================================================================
+    def typeText(self, message):
+        self.device.type(message)
     
     def installPkg(self, local_apk_path):
         self.device.installPackage(local_apk_path)
@@ -112,38 +104,16 @@ class MonkeyRunnerImpl():
     def startActivity(self, package_name, activity_name):
         runComponent = package_name + "/" + activity_name
         self.device.startActivity(component = runComponent)
+     
+    # Deletes the specified package from this device, including its data and cache.
+    def removePkg(self, package_name):
+        self.device.removePackage(package_name)
+    
+    # Wakes the screen of this device.
+    def wake(self):
+        self.device.wake()
         
-
-
-#===============================================================================
-# 
-#===============================================================================
-def testClickEvent(device):
-
-    device.press("KEYCODE_1", "DOWN_AND_UP")
-    MonkeyRunner.sleep(2)
-
-    device.press("KEYCODE_2", "DOWN_AND_UP")
-    MonkeyRunner.sleep(2)
-
-    device.press("KEYCODE_9", "DOWN_AND_UP")
-    MonkeyRunner.sleep(2)
-
-    device.press("KEYCODE_CLEAR", "DOWN_AND_UP")
-    MonkeyRunner.sleep(2)
-
-    device.press("KEYCODE_MENU", "DOWN_AND_UP")
-    MonkeyRunner.sleep(2)
-
-    device.press("KEYCODE_BACK", "DOWN_AND_UP")
-    MonkeyRunner.sleep(2)
-
-    device.press("KEYCODE_SEARCH", "DOWN_AND_UP")
-    MonkeyRunner.sleep(2)
-
-    #device.press("KEYCODE_HOME", "DOWN_AND_UP")        
         
-
 #===============================================================================
 # # Left: newLeft = (Root Node)->mLeft + (ParentNode)->mLeft + ... + self->mLeft
 # # Right: newRight = newLeft + (self->mRight - self->mLeft)
@@ -164,10 +134,10 @@ def clickEvent_Point(monkey_runner_impl):
     print "end"
     MonkeyRunner.sleep(1)
     
-    monkey_runner_impl.clickByKeyCode("KEYCODE_MENU", "DOWN_AND_UP");
+    monkey_runner_impl.press("KEYCODE_MENU", "DOWN_AND_UP");
     monkey_runner_impl.sleep(1)
     
-    monkey_runner_impl.clickByKeyCode("KEYCODE_BACK", "DOWN_AND_UP");
+    monkey_runner_impl.press("KEYCODE_BACK", "DOWN_AND_UP");
 
 def process_ViewFile(view_file):
     pass

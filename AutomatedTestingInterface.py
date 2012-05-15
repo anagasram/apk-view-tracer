77be5ee5a83ab67d4ade85f24e551987f9783971
makun <makun@cumt-makun-pc>
2012-05-16 01:01:39 +0800

    add some methods in AutomatedTestingInterface.py

--- AutomatedTestingInterface.py
+++ AutomatedTestingInterface.py
@@ -16,15 +16,47 @@ class AutomatedTestingInterface():
     CLOSED = None
     DELETE = None
     UP = None
-    DOWN = None
-    
-    
+    DOWN = None   
           
     def __init__(self):
         pass
     
-    def assertCurrentActivity(self):
+    def assertCurrentActivity(self, str_msg, str_name):
         pass
     
+    def clearEditText(self, editText):
+        pass
+    
+    def clickInList(self, objList, iIndex):
+        pass
+    
+    def clickLongInList(self, objList, iIndex):
+        pass
+    
+    def clickLongOnScreen(self, fX, fY):
+        pass
+    
+    def clickLongOnText(self, str_text): 
+        pass
+    
+    def clickOnToggleButton(self, str_name):
+        pass
+    
+    def clickOnView(self, objView):
+        pass
+    
+    def drag(self, fromX, fromY, toX, toY, iSteps):
+        pass
+    
+    def enterText(self, str_msg, objText):
+        pass
+    
+    def getCurrentActivity(self):
+        pass
+    
+    def getButton(self):
+        pass
+    
+      
     
     

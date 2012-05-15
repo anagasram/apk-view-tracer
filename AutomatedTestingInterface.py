421a42cc01497b0c62567d6b0244fcb2760f1042
kun_ma <kun_ma@nj-kun-ma.tw.trendnet.org>
2012-05-15 18:21:46 +0800

    add new class AutomatedTestingInterface.py

---
+++ AutomatedTestingInterface.py
@@ -0,0 +1,30 @@
+#! python2.7
+## -*- coding: utf-8 -*-
+
+## kun for Apk View Tracing
+## AutomatedTestingInterface.py
+
+import os
+
+class AutomatedTestingInterface():
+    '''
+    Interface for Automated Testing
+    '''
+    
+    ## class variables
+    OPENED = None
+    CLOSED = None
+    DELETE = None
+    UP = None
+    DOWN = None
+    
+    
+          
+    def __init__(self):
+        pass
+    
+    def assertCurrentActivity(self):
+        pass
+    
+    
+    
